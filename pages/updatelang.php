08e2e322e6c0a30ab3c5ebca2473a844bbd98cac
oshri <oshriro@matrix.co.il>
2017-04-30 08:45:01 +0300

    add updatelang.php

--- pages/updatelang.php
+++ pages/updatelang.php
@@ -1,21 +1,5 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
-/**
- * Created by PhpStorm.
- * User: oshri
- * Date: 23/04/17
- * Time: 08:20
- */
-
-
 
 $output = shell_exec(dirname(__FILE__).'/updatelang.sh');
 echo $output;
 
-//echo exec("sudo bash pages/updatelang.sh");
-
-
-
-die(1);
