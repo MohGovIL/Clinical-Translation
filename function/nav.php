b9961f37dc0913f33cf7c4f68ad3a508830872bc
oshri <oshriro@matrix.co.il>
2017-05-01 11:07:22 +0300

    fix lang bug

--- function/nav.php
+++ function/nav.php
@@ -7,7 +7,7 @@ function nav(){
                 <li role="presentation" id="inheritance"><a href="index.php?p=edit">ערוך תרגום</a></li>
                 <li role="presentation" id="list"><a href="index.php?p=add">הוסף תרגום</a></li>
                 <li role="presentation" id="list"><a href="index.php?p=upgradelang">ייבא תרגומים מהקהילה</a></li>
-                <li role="presentation" id="list"><a href="pages/exportlang.php">ייצא תרגום</a></li>
+                <li role="presentation" id="list"><a href="pages/exportlang.php">ייצא תרגומים</a></li>
             </ul>
         </div>
        </nav>
